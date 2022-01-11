c6ab28ebb3f9df5ed074332ea08407b972ee3004
kai <iftikharasjad@gmail.com>
2022-01-11 15:01:39 +0500

    Cleanup

--- single_page/admin.py
+++ single_page/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import houses, boss
+from .models import Houses, Boss
 
-admin.site.register(houses)
-admin.site.register(boss)
+admin.site.register(Houses)
+admin.site.register(Boss)
 
--- single_page/models.py
+++ single_page/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
-class houses(models.Model):
-    housename = models.CharField(max_length=100)
-    housepoints = models.IntegerField(max_length=100, null=True)
+
+class Houses(models.Model):
+    house_name = models.CharField(max_length=100)
+    house_points = models.IntegerField(max_length=100, null=True)
 
 
-class boss(models.Model):
+class Boss(models.Model):
     name = models.CharField(max_length=100)
     hp = models.IntegerField(max_length=100, null=True)
-    maxhp = models.IntegerField(max_length=100, null=True)
+    max_hp = models.IntegerField(max_length=100, null=True)
     gold = models.CharField(max_length=100)
