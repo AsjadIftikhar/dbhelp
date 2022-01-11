a0896b251de119a554ecae7186f710d32c27b34e
Ali Iqbal <ali.iqbal028@gmail.com>
2022-01-11 14:45:08 +0500

    the first model

--- single_page/views.py
+++ single_page/views.py
@@ -1,3 +1,42 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import houses,boss
 
-# Create your views here.
+
+
+def index(request):
+
+    housesList = list(houses.objects.all())
+    firstBoss = list(boss.objects.all())
+
+    print(housesList[0].housename)
+    print(firstBoss[0].name)
+
+    context = {'housesList': housesList}
+               # 'firstBoss': firstBoss}
+    # get_data = Data()
+    # Instances = get_data.get_instance_count()
+    # Jobs = get_data.get_jobs_count()
+    # Tasks = get_data.get_task_count()
+    # JobList = get_data.get_jobs()
+    # InstancesList = get_data.get_instances()
+    # TaskList = get_data.get_tasks()
+    #
+    # After_Week = datetime.date.today() + relativedelta(weeks=1)
+    # Now = datetime.date.today()
+    #
+    # context = {'segment': 'index',
+    #            'Instances': Instances,
+    #            'Jobs': Jobs,
+    #            'Tasks': Tasks,
+    #            'JobList': JobList,
+    #            'InstancesList': InstancesList,
+    #            'TaskList': TaskList,
+    #            'After_Week': After_Week,
+    #            'Now': Now}
+
+    # html_template = loader.get_template('home/index.html')
+    # return HttpResponse(html_template.render(context, request))
+    return HttpResponse("hello")
+
+
