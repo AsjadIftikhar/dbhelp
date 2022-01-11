991496853d2914d2bfaa44c7830ad592a2cd9e37
kai <iftikharasjad@gmail.com>
2022-01-11 17:22:48 +0500

    Completed

--- single_page/models.py
+++ single_page/models.py
@@ -5,9 +5,15 @@ class Houses(models.Model):
     house_name = models.CharField(max_length=100)
     house_points = models.IntegerField(null=True)
 
+    def __str__(self):
+        return self.house_name
+
 
 class Boss(models.Model):
     name = models.CharField(max_length=100)
     hp = models.IntegerField(null=True)
     max_hp = models.IntegerField(null=True)
     gold = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
--- single_page/views.py
+++ single_page/views.py
@@ -6,11 +6,18 @@ from .models import Houses, Boss
 
 
 def index(request):
-    houses_list = list(Houses.objects.all())
-    first_boss = list(Boss.objects.all())
+    houses_list = Houses.objects.all()
+    all_boss = Boss.objects.all()
+    boss = None
+    for b in all_boss:
+        boss = b
+        break
 
-    context = {'housesList': houses_list,
-               'firstBoss': first_boss}
+    percentage = (boss.hp / boss.max_hp)*100
+
+    context = {'houses_list': houses_list,
+               'boss': boss,
+               'percentage': percentage}
 
     html_template = loader.get_template('index.html')
     return HttpResponse(html_template.render(context, request))
